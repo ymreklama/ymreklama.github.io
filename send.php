c2b52b4b48cbdc13c27a55a40eb7a1d5cb1c03b7
Dima Chuhin <dime10cents@gmail.com>
2016-04-10 18:22:00 +0600

    ym

---
+++ send.php
@@ -0,0 +1,20 @@
+<?
+if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['email'])&&$_POST['email']!="")){ 
+        $to = 'dimae10cents@gmail.com'; //Почта получателя, через запятую можно указать сколько угодно адресов
+        $subject = 'Обратный звонок'; //Загаловок сообщения
+        $message = '
+                <html>
+                    <head>
+                        <title>'.$subject.'</title>
+                    </head>
+                    <body>
+                        <p>Имя: '.$_POST['name'].'</p>
+                        <p>Телефон: '.$_POST['email'].'</p>
+                        <p>'.$_POST['text'].'</p>                      
+                    </body>
+                </html>'; //Текст нащего сообщения можно использовать HTML теги
+        $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
+        $headers .= "From: Отправитель <from@example.com>\r\n"; //Наименование и почта отправителя
+        mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
+}
+?>
